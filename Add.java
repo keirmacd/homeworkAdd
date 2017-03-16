f1aa07f78c56f571b05bc46666912ee31b44539e
Keir Macdonald <keirmacd@hotmail.co.uk>
2017-03-16 10:58:01 +0000

    INITCOMMIT

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Write a program that asks the user for a number n and prints the sum of the numbers 1 to n. 
+//So for example if user enters 5 it will print out 15. (1+2+3+4+5)
+
+public class Add {
+      
+      public static void main(String[] args) {
+        System.out.println("Enter in a number: ");
+        Scanner sc = new scanner();
+        int input = scanner.nextInt();
+        System.out.println(input)
+
+        for (int input =  
+
+      }
+}
